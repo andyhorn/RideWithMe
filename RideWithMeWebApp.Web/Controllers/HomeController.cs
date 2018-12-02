b9e21119810fafe8ced572f1897f4286e0489793
Andy <andyjhorn@gmail.com>
2018-12-02 00:08:26 -0800

    Substantial progress on front-end. User can successfully login and have their data passed between controller and view.

--- RideWithMeWebApp.Web/Controllers/HomeController.cs
+++ RideWithMeWebApp.Web/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using RideWithMeWebApp.Models.Classes;
 using RideWithMeWebApp.Models.Interfaces;
+using WebRequest = System.Net.WebRequest;
 
 namespace RideWithMeWebApp.Web.Controllers
 {
@@ -12,28 +13,72 @@ namespace RideWithMeWebApp.Web.Controllers
     {
         private IUser _currentUser;
 
+        private const string URL = "https://ridewithmeapp.azurewebsites.net/api/";
+
         public HomeController()
         {
-            _currentUser = new User();
+            _currentUser = null;
+        }
+
+        public ActionResult Login()
+        {
+            if (_currentUser == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            _currentUser = null;
+            ViewBag.User = null;
+            ViewBag.Vehicles = null;
+            ViewBag.Rides = null;
+
+            return RedirectToAction("Index");
+        }
+
+        private Models.WebResponse GetResponse(string url)
+        {
+            var webRequest = WebRequest.Create(url);
+            webRequest.ContentType = "application/json; charset=utf-8";
+            var response = (HttpWebResponse)webRequest.GetResponse();
+            var dataStream = response.GetResponseStream();
+            if (dataStream != null)
+            {
+                var reader = new StreamReader(dataStream);
+                var serverResponse = reader.ReadToEnd();
+                var json = JsonConvert.DeserializeObject<Models.WebResponse>(serverResponse);
+                return json;
+            }
+
+            return null;
+        }
+
+        public ActionResult SubmitLogin()
+        {
+            var email = Request.QueryString["Email"];
+            var password = Request.QueryString["Password"];
+
+            var result = GetResponse(URL + $"login?email={email}&password={password}");
+            if (result != null && result.Message == "Success!")
+            {
+                var user = JsonConvert.DeserializeObject<User>(result.Data["user"].ToString());
+                ViewBag.Status = 0;
+                ViewBag.User = user;
+                _currentUser = user;
+                return View("Index");
+            }
+
+            ViewBag.Status = -1;
+            return View("Login");
         }
         public ActionResult Index()
         {
-            _currentUser.UserType = 0;
-            ViewData["user"] = _currentUser;
-            return View(_currentUser);
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ RideWithMeWebApp.Web/Models/WebResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RideWithMeWebApp.Web.Models
+{
+    public class WebResponse
+    {
+        public string Message { get; set; }
+        public Dictionary<string, object> Data { get; }
+
+        public WebResponse()
+        {
+            Data = new Dictionary<string, object>();
+        }
+    }
+}
