6affab6f892eef253c67ee27f2ce82acd8334b57
Andy <andyjhorn@gmail.com>
2018-12-01 18:56:45 -0800

    Header and Footer partial layouts complete.

--- RideWithMeWebApp.Web/Controllers/HomeController.cs
+++ RideWithMeWebApp.Web/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RideWithMeWebApp.Models.Classes;
+using RideWithMeWebApp.Models.Interfaces;
 
 namespace RideWithMeWebApp.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IUser _currentUser;
+
+        public HomeController()
+        {
+            _currentUser = new User();
+        }
         public ActionResult Index()
         {
-            return View();
+            _currentUser.UserType = 0;
+            ViewData["user"] = _currentUser;
+            return View(_currentUser);
         }
 
         public ActionResult About()
